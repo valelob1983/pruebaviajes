b505568a141e0c77d0b13fd2a14bc75727dfb7f4
unknown <vic.valesca@gmail.com>
2024-07-11 23:58:18 -0400

    seccion quienes somos y destacados

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,3 +3,12 @@ function smoothScroll(target) {
         behavior: 'smooth'
     });
 }
+
+$(document).ready(function(){
+    $('[data-toggle="popover"]').popover();
+});
+
+var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'));
+var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+    return new bootstrap.Popover(popoverTriggerEl);
+});
